2d85c60eca8ff0d2da47bc8acd6eb11a0aa203a0
Owen Hooper <17ofh@queensu.ca>
2023-11-27 21:21:43 -0500

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+# set --universal tide_left_prompt_items distrobox $tide_left_prompt_items
+if test (lsb_release -sc) = "jammy"; 
+echo JAMMY
+export XDG_DATA_HOME="/home/owen/.local/share20" 
+export XDG_DATA_DIRS=/usr/share/regolith:/usr/share/gnome:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop:/home/owen/.local/share20
+else if test (lsb_release -sc) = "bionic";
+export XDG_DATA_HOME="/home/owen/.local/share18" 
+export XDG_DATA_DIRS=/usr/share/regolith:/usr/share/gnome:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop:/home/owen/.local/share18
+else  
+export ROS_DOMAIN_ID=42
+alias ls="eza --icons=always --hyperlink"
+end 
+fish_add_path -a --path /home/owen/appimages
+register-python-argcomplete --shell fish pipx | source
+register-python-argcomplete --shell fish ros2 | source
+
+
+set fish_greeting You have $(math $(ps aux | grep /usr/bin/fish | wc -l) - 1) shells open
+
+alias roscd="ccd"
+
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+
+theme_tokyonight night
+
+fish_add_path /usr/lib/ccache/bin/
+fish_add_path /home/owen/bin
+alias vi nvim
+    # Commands to run in interactive sessions can go here
+end
+
