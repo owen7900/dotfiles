93b5f22a94d09778528759ae5ca05ff6363ad463
Owen Hooper <17ofh@queensu.ca>
2023-11-30 22:17:22 -0500

    fixed command line args

--- .config/hypr/tofi-run.bash
+++ .config/hypr/tofi-run.bash
@@ -1,19 +1,28 @@
 #! /bin/bash
-#
+
 tfa="--font /usr/share/fonts/TTF/JetBrainsMonoNerdFontMono-Medium.ttf -c $HOME/.config/tofi.theme --require-match false --history-file $HOME/.local/state/tofi-custom-history"
 tf="tofi $tfa"
 arg=$( { awk 'BEGIN{FS="="}; /^GenericName[^\[\]]/ || /^Name[^\[\]]/ {print $2}' /usr/share/applications/*.desktop $HOME/.local/share/applications/*.desktop; compgen -c; } | $tf)
 if [[ $arg =~ ^[0-9] ]]; then
   ans=$(calc "$arg") 
-  echo "" | $tf --prompt-text $ans
+  if [[ -z $ans ]]; then
+    ans="Err"
+  fi
+  calc $arg 2> >($tf --prompt-text $ans)
 elif [[ ! -z $arg ]]; then
-  echo $arg
-  file=$(grep -l "^.*Name=$arg" /usr/share/applications/*.desktop $HOME/.local/share/applications/*.desktop)
+  files=$(grep -l "^.*Name=$arg" /usr/share/applications/*.desktop $HOME/.local/share/applications/*.desktop)
+  file=""
+  if [[ ! -z $files ]]; then
+    file=$(grep -L "NoDisplay=true" $files)
+  fi
   if [[ -f $file ]]; then
-    echo "file: $file"
-    exe=$(awk 'BEGIN{FS="="}/Exec/{print$2}' $file)
+    exe=$(awk 'BEGIN{FS="[=\ ]"}/^Exec/{print$2}' $file)
     if type $exe >/dev/null 2>&1; then
-      $exe &
+      if grep -q "Terminal=true" $file; then
+        kitty --hold fish -c "$exe"
+      else
+        $exe &
+      fi
     else 
       notify-send "Could not find executable for $arg in $file found $exe"
     fi
