66b4f1b0c6c73920b3cc50970ac9258c46d968f7
Owen Hooper <ohooper@deeptrekker.com>
2023-12-04 11:00:57 -0500

    Added debugger config from work computer

--- .config/nvim.user/init.lua
+++ .config/nvim.user/init.lua
@@ -1,50 +1,102 @@
 return {
-  plugins = {
-    {
-      "goolord/alpha-nvim",
-      opts = function(_, opts)      -- override the options using lazy.nvim
-        opts.section.header.val = { -- change the header section value
-          " ██████  ██     ██ ███████ ███    ██ ███████     ███    ██ ███████  ██████  ██    ██ ██ ███    ███      ██████  ██████  ███    ██ ███████ ██  ██████  ",
-          "██    ██ ██     ██ ██      ████   ██ ██          ████   ██ ██      ██    ██ ██    ██ ██ ████  ████     ██      ██    ██ ████   ██ ██      ██ ██       ",
-          "██    ██ ██  █  ██ █████   ██ ██  ██ ███████     ██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██     ██      ██    ██ ██ ██  ██ █████   ██ ██   ███ ",
-          "██    ██ ██ ███ ██ ██      ██  ██ ██      ██     ██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██     ██      ██    ██ ██  ██ ██ ██      ██ ██    ██ ",
-          " ██████   ███ ███  ███████ ██   ████ ███████     ██   ████ ███████  ██████    ████   ██ ██      ██      ██████  ██████  ██   ████ ██      ██  ██████  "
-        }
-      end,
-    },
-  },
-  icons = {
-    MacroRecording = "",
-    ActiveLSP = "",
-    ActiveTS = "綠",
-    BufferClose = "",
-    NeovimClose = "",
-    DefaultFile = "",
-    Diagnostic = "裂",
-    DiagnosticError = "",
-    DiagnosticHint = "",
-    DiagnosticInfo = "",
-    DiagnosticWarn = "",
-    Ellipsis = "…",
-    FileModified = "",
-    FileReadOnly = "",
-    FolderClosed = "",
-    FolderEmpty = "",
-    FolderOpen = "",
-    Git = "",
-    GitAdd = "",
-    GitBranch = "",
-    GitChange = "",
-    GitConflict = "",
-    GitDelete = "",
-    GitIgnored = "◌",
-    GitRenamed = "➜",
-    GitStaged = "✓",
-    GitUnstaged = "✗",
-    GitUntracked = "★",
-    LSPLoaded = "",
-    LSPLoading1 = "",
-    LSPLoading2 = "",
-    LSPLoading3 = "",
-  },
+	plugins = {
+		{
+			"goolord/alpha-nvim",
+			opts = function(_, opts) -- override the options using lazy.nvim
+				opts.section.header.val = { -- change the header section value
+					" ██████  ██     ██ ███████ ███    ██ ███████     ███    ██ ███████  ██████  ██    ██ ██ ███    ███      ██████  ██████  ███    ██ ███████ ██  ██████  ",
+					"██    ██ ██     ██ ██      ████   ██ ██          ████   ██ ██      ██    ██ ██    ██ ██ ████  ████     ██      ██    ██ ████   ██ ██      ██ ██       ",
+					"██    ██ ██  █  ██ █████   ██ ██  ██ ███████     ██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██     ██      ██    ██ ██ ██  ██ █████   ██ ██   ███ ",
+					"██    ██ ██ ███ ██ ██      ██  ██ ██      ██     ██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██     ██      ██    ██ ██  ██ ██ ██      ██ ██    ██ ",
+					" ██████   ███ ███  ███████ ██   ████ ███████     ██   ████ ███████  ██████    ████   ██ ██      ██      ██████  ██████  ██   ████ ██      ██  ██████  ",
+				}
+			end,
+		},
+		{
+			"jay-babu/mason-nvim-dap.nvim",
+			opts = {
+				handlers = {
+					cppdbg = function(source_name)
+						local dap = require("dap")
+						dap.configurations.cpp = {
+							{
+								type = "cppdbg",
+								request = "launch",
+								name = "Launch Firmware",
+								MIMode = "gdb",
+								miDebuggerServerAddress = "localhost:61234",
+								miDebuggerPath = "/usr/bin/gdb-multiarch",
+								cwd = "${workspaceFolder}",
+								program = function()
+									return vim.fn.input(
+										"Path to executable: ",
+										vim.fn.getcwd() .. "/build/f_build/",
+										"file"
+									)
+								end,
+							},
+							{
+								name = "Attach to gdbserver :1234",
+								type = "cppdbg",
+								request = "launch",
+								MIMode = "gdb",
+								miDebuggerServerAddress = function()
+									return vim.fn.input("Address")
+								end,
+								miDebuggerPath = "/usr/bin/gdb-multiarch",
+								cwd = "${workspaceFolder}",
+								program = function()
+									return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+								end,
+							},
+							{
+								name = "Launch file",
+								type = "cppdbg",
+								request = "launch",
+								program = function()
+									return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+								end,
+								cwd = "${workspaceFolder}",
+								stopAtEntry = true,
+							},
+						}
+					end,
+				},
+			},
+		},
+	},
+	icons = {
+		MacroRecording = "",
+		ActiveLSP = "",
+		ActiveTS = "綠",
+		BufferClose = "",
+		NeovimClose = "",
+		DefaultFile = "",
+		Diagnostic = "裂",
+		DiagnosticError = "",
+		DiagnosticHint = "",
+		DiagnosticInfo = "",
+		DiagnosticWarn = "",
+		Ellipsis = "…",
+		FileModified = "",
+		FileReadOnly = "",
+		FolderClosed = "",
+		FolderEmpty = "",
+		FolderOpen = "",
+		Git = "",
+		GitAdd = "",
+		GitBranch = "",
+		GitChange = "",
+		GitConflict = "",
+		GitDelete = "",
+		GitIgnored = "◌",
+		GitRenamed = "➜",
+		GitStaged = "✓",
+		GitUnstaged = "✗",
+		GitUntracked = "★",
+		LSPLoaded = "",
+		LSPLoading1 = "",
+		LSPLoading2 = "",
+		LSPLoading3 = "",
+	},
 }
