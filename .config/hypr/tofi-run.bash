df78e50530fb32243365902a5139f45b6d36a055
Owen Hooper <17ofh@queensu.ca>
2023-12-26 12:54:08 -0500

    Added custom run mode to have hex dec and oct conversions

--- .config/hypr/tofi-run.bash
+++ .config/hypr/tofi-run.bash
@@ -3,7 +3,38 @@
 tfa="--font /usr/share/fonts/TTF/JetBrainsMonoNerdFontMono-Medium.ttf -c $HOME/.config/tofi.theme --require-match false --history-file $HOME/.local/state/tofi-custom-history"
 tf="tofi $tfa"
 arg=$( { awk 'BEGIN{FS="="}; /^GenericName[^\[\]]/ || /^Name[^\[\]]/ {print $2}' /usr/share/applications/*.desktop $HOME/.local/share/applications/*.desktop; compgen -c; } | $tf)
-if [[ $arg =~ ^[0-9] ]]; then
+getibase() {
+  if [[ $1 =~ ^0x ]]; then
+    echo "16"
+  elif [[ $1 =~ ^0o ]]; then 
+    echo "8"
+  else 
+    echo "10"
+  fi
+}
+stripPrefix() {
+  if [[ $1 =~ ^0[a-z] ]]; then
+    echo `echo $1 | cut -c 3-`
+  else 
+    echo $1
+  fi
+}
+if [[ $arg =~ ^hex\  ]]; then
+    val=`echo $arg | awk 'BEGIN{FS=" "};{print $2}'`
+    ibase=`getibase $val`
+    val=`stripPrefix $val`
+    echo "0x"`echo "obase=16;ibase=$ibase; $val" | bc` | $tf
+elif [[ $arg =~ ^dec\  ]]; then
+    val=`echo $arg | awk 'BEGIN{FS=" "};{print $2}'`
+    ibase=`getibase $val`
+    val=`stripPrefix $val`
+    echo "obase=10; ibase=$ibase; $val" | bc | $tf
+elif [[ $arg =~ ^oct\  ]]; then
+    val=`echo $arg | awk 'BEGIN{FS=" "};{print $2}'`
+    ibase=`getibase $val`
+    val=`stripPrefix $val`
+    echo "0o"`echo "obase=8;ibase=$ibase; $val" | bc` | $tf
+elif [[ $arg =~ ^[0-9] ]]; then
   ans=$(calc "$arg") 
   if [[ -z $ans ]]; then
     ans="Err"
