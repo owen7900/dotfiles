15790b6288bea94a692f8aa5afcc4cb5496fdcae
Owen Hooper <ohooper@deeptrekker.com>
2025-01-02 12:12:18 -0500

    Changes for lazy's move to fzf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,20 @@
 return {
-  "nvim-telescope/telescope.nvim",
+  "ibhagwan/fzf-lua",
   keys = {
     -- disable the keymap to grep files
     { "<leader>/", false },
     -- change a keymap
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
     -- add a keymap to browse plugin files
     {
       "<leader>fw",
-      "<cmd>Telescope live_grep<cr>",
+      "<cmd>FzfLua live_grep<cr>",
       desc = "Live grep",
     },
     {
       "<leader>fs",
       --"<cmd>Telescope lsp_document_symbols<cr>",
-      "<cmd>Telescope treesitter<cr>",
+      "<cmd>FzfLua lsp_document_symbols<cr>",
       desc = "Find Doc Symbols",
     },
   },
